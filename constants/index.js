3b5b976b2885a792928bd4813bf7fcce0e88b2f3
Eveningstar87 <108192164+Eveningstar87@users.noreply.github.com>
2023-08-01 16:10:06 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x863b7fbc9cdb6857e2e56b64474bdcb76b47965b";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
